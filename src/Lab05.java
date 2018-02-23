2dc4226b62d3a9650eddb737b4274efd0415050d
aubreyjcook <aubreyjcook@gmail.com>
2018-02-23 17:11:07 -0600

    basic class/method set up

--- src/Lab05.java
+++ src/Lab05.java
@@ -1,4 +1,14 @@
+import java.util.*;
 
 public class Lab05 {
-
+	
+	public static void main(String[] args) {
+		new Lab05();
+	}
+	
+	public Lab05() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number 1-26: ");
+		
+	}
 }
