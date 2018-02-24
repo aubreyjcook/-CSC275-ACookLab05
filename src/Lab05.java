bcaa6ece367b8a857aecedb09219c8ae454a662c
aubreyjcook <aubreyjcook@gmail.com>
2018-02-24 15:16:16 -0600

    InputMismatchException implemented

--- src/Lab05.java
+++ src/Lab05.java
@@ -3,12 +3,18 @@ import java.util.*;
 public class Lab05 {
 	
 	public static void main(String[] args) {
-		new Lab05();
-	}
-	
-	public Lab05() {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a number 1-26: ");
+		boolean continueInput = true;
 		
+		do {
+			try {
+				System.out.println("Enter a number 1-26: ");
+				int userInput = input.nextInt();
+			}
+			catch(InputMismatchException ex) {
+				System.out.println("Please input a number.");
+				input.nextLine();
+			}
+		} while(continueInput);
 	}
 }
