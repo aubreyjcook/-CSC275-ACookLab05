e4da9d028f36274960cf8dbb18b4388611f6cb97
aubreyjcook <aubreyjcook@gmail.com>
2018-02-26 03:28:23 -0600

    number range detection logic added
    if statement added to the try/catch do while loop to determine if an int is within the given range needed

--- src/Lab05.java
+++ src/Lab05.java
@@ -4,14 +4,16 @@ public class Lab05 {
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		int userInputNumber = 0;
 		boolean numberRetrieved = false;
 		
 		do {
 			try {
 				System.out.println("Enter a number 1-26: ");
-				int userInputNumber = input.nextInt();
+				userInputNumber = input.nextInt();
 				
-				numberRetrieved = true;
+				if(!(userInputNumber < 1 || userInputNumber > 26)) {numberRetrieved = true;} else {System.out.println("Bad input.");}
+				
 			}
 			catch(InputMismatchException ex) {
 				System.out.println("Bad input.");
@@ -22,13 +24,12 @@ public class Lab05 {
 		System.out.println("Enter a phrase: ");
 		String userInputPhrase = input.next();
 		
-		String newPhrase = shiftPhrase(userInputPhrase);
+		String newPhrase = shiftPhrase(userInputPhrase, userInputNumber);
 		System.out.println(newPhrase);
 	}
 	
-	private String shiftPhrase(String userInputPhrase) {
-		String newPhrase;
-		newPhrase = userInputPhrase;
+	private static String shiftPhrase(String userInputPhrase, int userInputNumber) {
+		char[] newPhrase = userInputPhrase.toCharArray();
 		
 		return newPhrase;
 	}
