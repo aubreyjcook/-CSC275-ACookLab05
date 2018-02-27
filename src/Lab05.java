c294abb85d2980180e05ab5a05ee11db1f2210eb
aubreyjcook <aubreyjcook@gmail.com>
2018-02-27 11:31:57 -0600

    shift phrase conditional logic rot cipher implemented

--- src/Lab05.java
+++ src/Lab05.java
@@ -32,17 +32,47 @@ public class Lab05 {
 	
 	private static String shiftPhrase(String userInputPhrase, int userInputNumber) {
 		char[] newPhrase = userInputPhrase.toCharArray();
-		for(int i = 0; i < userInputPhrase.length(); i++) {
-			char rot = newPhrase[i];
+		
+		if(userInputNumber > 13) {
+			int remainder = userInputNumber - 13;
 			
-			//fix this with rot-n
-			if       (rot >= 'a' && rot <= 'm') rot += 13;
-            else if  (rot >= 'A' && rot <= 'M') rot += 13;
-            else if  (rot >= 'n' && rot <= 'z') rot -= 13;
-            else if  (rot >= 'N' && rot <= 'Z') rot -= 13;
-            
-			newPhrase[i] = rot;
+			for(int i = 0; i < userInputPhrase.length(); i++) {
+				char rot = newPhrase[i];
+				
+				if       (rot >= 'a' && rot <= 'm') rot += remainder;
+	            else if  (rot >= 'A' && rot <= 'M') rot += remainder;
+	            else if  (rot >= 'n' && rot <= 'z') rot -= remainder;
+	            else if  (rot >= 'N' && rot <= 'Z') rot -= remainder;
+	            
+				newPhrase[i] = rot;
+			}
+			
+			userInputNumber = userInputNumber - 13;
+			
+			for(int i = 0; i < userInputPhrase.length(); i++) {
+				char rot = newPhrase[i];
+							
+				if       (rot >= 'a' && rot <= 'm') rot += userInputNumber;
+	            else if  (rot >= 'A' && rot <= 'M') rot += userInputNumber;
+	            else if  (rot >= 'n' && rot <= 'z') rot -= userInputNumber;
+	            else if  (rot >= 'N' && rot <= 'Z') rot -= userInputNumber;
+	            
+				newPhrase[i] = rot;
+			}
+		} else {
+			for(int i = 0; i < userInputPhrase.length(); i++) {
+				char rot = newPhrase[i];
+							
+				if       (rot >= 'a' && rot <= 'm') rot += userInputNumber;
+	            else if  (rot >= 'A' && rot <= 'M') rot += userInputNumber;
+	            else if  (rot >= 'n' && rot <= 'z') rot -= userInputNumber;
+	            else if  (rot >= 'N' && rot <= 'Z') rot -= userInputNumber;
+	            
+				newPhrase[i] = rot;
+			}
 		}
+						
+		
 		return String.valueOf(newPhrase);
 	}
 }
